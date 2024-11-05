3bb903d9779bd712ecbc510097d25e47f270d7df
bhuvaneswar <bhuvaneswarr412@gmail.com.com>
2024-11-05 10:06:37 +0530

    first commit

---
+++ src/main/java/com/drucare/DrucareProjectApplication.java
@@ -0,0 +1,13 @@
+package com.drucare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrucareProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrucareProjectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/drucare/ReportsController.java
@@ -0,0 +1,14 @@
+package com.drucare;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReportsController {
+
+	@GetMapping("/")
+	public String welcome() {
+		return "Welcome to reports!";
+	}
+
+}
